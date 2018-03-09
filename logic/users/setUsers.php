46fae792e27cba2729dd668bcd59ce6ddba69c17
Md. Nashid Kamal Jitu <nashidkamaljitu7@gmail.com>
2018-03-09 06:29:25 +0600

    Registration validation/ js

---
+++ logic/users/setUsers.php
@@ -0,0 +1,5 @@
+<?php
+	function setUsers($username, $password, $name, $type = "customer"){
+		
+	}
+?>
--- views/register.php
+++ views/register.php
@@ -2,17 +2,37 @@
 	<head>
 		<title>Register Yourself</title>
 	</head>
+	<script type="text/javascript">
+		function validate(){
+			var username = document.forms["registrationform"]["username"];
+			var password = document.forms["registrationform"]["password"];
+			var confirmpassword = document.forms["registrationform"]["confirmpassword"];
+			var name = document.forms["registrationform"]["name"];
+			var errorMsg = document.getElementById("errorMsg");
 
+			if(username.value == "" || password.value == "" || confirmpassword.value == "" || name.value == ""){
+				errorMsg.textContent = "All fields required.";
+				return false;
+			}else if(password.value != confirmpassword.value){
+					errorMsg.textContent = "Passwords didn't matched";
+				return false;
+			}else{
+				errorMsg.textContent = "";
+				return true;
+			}
+		}
+	</script>
 	<body>
 		<div id="logo" align="center">
 			<a href="../index.php"><h1>LOGO<br/></h1></a>
 			<hr size="3"/>
 		</div>
 		<div id="registrationform">
-			<form action="../logic/authenticate/register.php" method="post">
+			<form action="../logic/authenticate/register.php" onsubmit="return validate()" method="post" name="registrationform">
 				<fieldset>
 					<legend align="center"><h3>Register for an account</h3></legend>
 					<table align="center" valign="top">
+						<tr><td colspan="3"><div align="center" style="color: RED" id="errorMsg"></div></td></tr>
 						<tr>
 							<td><strong>Name</strong></td>
 							<td><strong>:</strong></td>
